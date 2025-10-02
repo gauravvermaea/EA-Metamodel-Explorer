dce9166eff0731fe86737976a11b0eae06c2e63d
gauravvermaea <gaurav.verma.ea@outlook.com>
2025-10-02 16:31:51 +0200

    added the plugin file

--- 0002_POCs/0002_Custom_Draw_IO_Plugin_To_Post_Data_To_API/code/add_menu_item_plugin.js
+++ 0002_POCs/0002_Custom_Draw_IO_Plugin_To_Post_Data_To_API/code/add_menu_item_plugin.js
@@ -1,17 +1,30 @@
 Draw.loadPlugin(function(ui) {
-
-    alert('Plugin Loading');
-    // Define an action
+    // 1. Register a new action
     ui.actions.addAction('showAlert', function() {
-        alert('Hello from My Plugin!');
+        alert('Hello from my custom plugin!');
     });
 
-    // Create a new top-level menu
-    ui.menubar.addMenu('My Plugin', function(menu, parent) {
-        ui.menubar.addMenuItem(menu, 'showAlert');
+
+
+const apiUrl = 'https://api.restful-api.dev/objects'; // Replace with your actual API endpoint
+
+// Make a GET request
+
+
+     ui.actions.addAction('call_get', function() {
+         fetch(apiUrl)
+        .then((response) => response.json())
+        .then((json) => console.log(json));
     });
-    
-    alert('Plugin Loaded');
-    
-}
-);
+
+    // 2. Add it into an existing menu ("extras" works well)
+    ui.menus.get('extras').funct = (function(oldFunct){
+        return function(menu, parent) {
+            // Call the original menu builder
+            oldFunct.apply(this, arguments);
+            // Add our custom menu item at the end
+            ui.menus.addMenuItem(menu, 'showAlert', parent);
+            ui.menus.addMenuItem(menu, 'call_get', parent);
+        };
+    })(ui.menus.get('extras').funct);
+});
